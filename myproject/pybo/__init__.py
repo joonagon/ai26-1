e0530cea6ca86e96340faac086f921ea4658c4f6
dla9944 <dla9944@naver.com>
2023-04-08 20:56:29 +0900

    ORM 라이브러리, SQLite, SQL Alchemy 사용 모델 생성

--- myproject/pybo/__init__.py
+++ myproject/pybo/__init__.py
@@ -1,7 +1,24 @@
+
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+import config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__)  # flask 뷰 기본
+    
+    # ORM을 적용하기 위해서 SQLite와 SQLAlchemy 사용
+    app.config.from_object(config)
+
+    #  ORM
+    db.init_app(app)
+    migrate.init_app(app, db)
+    from . import models
 
     # 블루프린트로 라우팅함수 관리함
     from .views import main_views
